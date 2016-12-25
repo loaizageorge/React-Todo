5f9d22e1646f07c885abc6d1511f69cb79aa2b4f
George <loaizageorge@gmail.com>
2016-12-25 18:58:13 -0500

    Add redux store

---
+++ app/actions/actions.jsx
@@ -0,0 +1,30 @@
+export var setSearchText = (searchText) => {
+  return {
+    type: 'SET_SEARCH_TEXT',
+    searchText
+  };
+};
+
+// toggleShowCompleted
+export var toggleShowCompleted = () => {
+  return {
+    type: 'TOGGLE_SHOW_COMPLETED'
+  };
+};
+
+export var addTodo = (text) => {
+  return {
+    type: 'ADD_TODO',
+    text
+  };
+};
+
+export var toggleTodo = (id) => {
+  return {
+    type: 'TOGGLE_TODO',
+    id
+  };
+};
+
+
+// toggleTodo(id)
